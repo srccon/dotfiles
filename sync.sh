79bc8d4bf0cece4372e4d974032b3443aa2249eb
Alexander J. Dita <info@alexanderjdita.com>
2017-03-31 17:24:46 -0700

    clarified comments

--- sync.sh
+++ sync.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Where do you want the dotfiles directory
+# Where do you want the dotfiles directory?
 dotfilepwd="$HOME/.dotfiles"
 
+# How about the original/default dotfiles?
 dotfileorig="$dotfilepwd/orig"
-
-syncfile="sync.sh"
 
 # Temp location
 tmpfile="/tmp/dotfiles.tmp"
 
-if [ ! -d $dotfileorig ]; then
+if [ ! -d "$dotfileorig" ]; then
 	echo "Making $dotfileorig directory for existing files/directories"
 	mkdir $dotfileorig
 fi
 
-find -maxdepth 1 | sed 's/\.\///' | tail -n +2 | grep -v ^$syncfile | grep -v ^orig | grep -v ^.git > "$tmpfile"
+find -maxdepth 1 | sed 's/\.\///' | tail -n +2 | grep -v ^sync.sh$ | grep -v ^orig$ | grep -v ^.git | grep -v ^lib$ > "$tmpfile"
 
 while read i; do
 	if [ -a "$HOME/$i" -a ! -h "$HOME/$i" ]; then
