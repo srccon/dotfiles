370ef77fca76a79ca6558ec3cda869e372f84c49
Alexander J. Dita <info@alexanderjdita.com>
2017-04-02 22:00:19 -0700

    removed completion from apt aliases because of error

--- .zsh.d/apt.zsh
+++ .zsh.d/apt.zsh
@@ -112,39 +112,6 @@ else
         ?not(~n`uname -r`))'\'' root'
 fi
 
-# Completion ################################################################
-
-#
-# Registers a compdef for $1 that calls $apt_pref with the commands $2
-# To do that it creates a new completion function called _apt_pref_$2
-#
-apt_pref_compdef() {
-    local f fb
-    f="_apt_pref_${2}"
-
-    eval "function ${f}() {
-        shift words; 
-	service=\"\$apt_pref\"; 
-	words=(\"\$apt_pref\" '$2' \$words); 
-	((CURRENT++))
-	test \"\${apt_pref}\" = 'aptitude' && _aptitude || _apt
-    }"
-
-    compdef "$f" "$1"
-}
-
-apt_pref_compdef aac "autoclean"
-apt_pref_compdef abd "build-dep"
-apt_pref_compdef ac  "clean"
-apt_pref_compdef ad  "update"
-apt_pref_compdef afu "update"
-apt_pref_compdef ag  "$apt_upgr"
-apt_pref_compdef ai  "install"
-apt_pref_compdef ail "install"
-apt_pref_compdef ap  "purge"
-apt_pref_compdef ar  "remove"
-apt_pref_compdef ads "dselect-upgrade"
-
 # Misc. #####################################################################
 # print all installed packages
 alias allpkgs='aptitude search -F "%p" --disable-columns ~i'
