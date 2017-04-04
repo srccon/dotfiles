7801e6af5fceee5e1406793276e54e7df7afe199
Alexander J. Dita <info@alexanderjdita.com>
2017-04-04 13:31:00 -0700

    changed path for .motd.d

--- .sh.d/motd.sh
+++ .sh.d/motd.sh
@@ -1,3 +1,3 @@
 # MOTD
 # Nice greeting when opening a terminal window
-if [[ -d "$DOTDIR/.motd.d" ]];then for f in `ls $HOME/.motd.d/*`; do source $f; done; fi
+if [[ -d "$DOTDIR/.motd.d" ]];then for f in `ls $DOTDIR/.motd.d/*`; do source $f; done; fi
